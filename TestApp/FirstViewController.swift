d3fa3e85467a991713a1b8984d3947f3ec2f8205
s-okumura <gedantgruv_s.o@hotmail.co.jp>
2015-10-12 08:34:13 +0900

    初回コミット。
    ボタンとラベルを新規作成。
    ボタンをタップすると色が変わるメソッドを新規作成。

--- TestApp/FirstViewController.swift
+++ TestApp/FirstViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var label1: UILabel!
+
+    @IBAction func button1TouchDown(sender: AnyObject) {
+        label1.textColor = UIColor.redColor()
+    }
+    
+    @IBAction func button1TouchDownRepeat(sender: AnyObject) {
+        label1.textColor = UIColor.blackColor()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
